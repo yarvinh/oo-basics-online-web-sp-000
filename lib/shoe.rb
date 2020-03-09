7858fb321eb16c7082f0e493837db61e75d91504
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-09 21:00:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,7 +11,7 @@ class Shoe
      puts "Your shoe is as good as new!"
   end
   def condition
-    if @cobble
+    if @cobble == "Your shoe is as good as new!"
       @condition = "new"
     end
   end
